e3a87e921a03ac93f81d634acde9283fd11d650b
Gnana Priya N <gnanapriya.namasivayam@syncfusion.com>
2019-08-05 09:22:11 +0530

    Uploaded sample and read me file

---
+++ ListViewMVVM/ListViewMVVM/ListViewMVVM/ViewModel/BookInfoRepository.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MVVM
+{
+    public class BookInfoRepository : INotifyPropertyChanged
+    {
+        private ObservableCollection<BookInfo> bookInfoCollection;
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ObservableCollection<BookInfo> BookInfoCollection
+        {
+            get { return bookInfoCollection; }
+            set
+            {
+                this.bookInfoCollection = value;
+                this.OnPropertyChanged("BookInfoCollection");
+            }
+        }
+
+        private Command holdCommand;
+
+        public Command HoldCommand
+        {
+            get { return holdCommand; }
+            protected set { holdCommand = value; }
+        }
+
+        public void OnPropertyChanged(string name)
+        {
+            if (this.PropertyChanged != null)
+                this.PropertyChanged(this, new PropertyChangedEventArgs(name));
+        }
+
+        public BookInfoRepository()
+        {
+            GenerateNewBookInfo();
+            holdCommand = new Command(OnItemHold);
+        }
+
+        private void GenerateNewBookInfo()
+        {
+            BookInfoCollection = new ObservableCollection<BookInfo>();
+            BookInfoCollection.Add(new BookInfo() { BookName = "Machine Learning Using C#", BookDescription = "You’ll learn several different approaches to applying machine learning" });
+            BookInfoCollection.Add(new BookInfo() { BookName = "Object-Oriented Programming in C#", BookDescription = "Object-oriented programming is the de facto programming paradigm" });
+            BookInfoCollection.Add(new BookInfo() { BookName = "C# Code Contracts", BookDescription = "Code Contracts provide a way to convey code assumptions" });
+        }
+
+        ///<summary>
+        /// Displays the item holding content
+        ///</summary>
+        public void OnItemHold(object obj)
+        {
+            var eventArgs = obj as Syncfusion.ListView.XForms.ItemHoldingEventArgs;
+            Application.Current.MainPage.DisplayAlert("Type Of Item :" + eventArgs.ItemType, "Item Tapped Position : + " + eventArgs.Position, "Ok");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,39 @@
-# Xamarin-forms-listview-itemholding-
-Xamarin-forms-listview-itemholding 
+# Handling ItemHolding event in ListView
+
+ListView supports binding the [HoldCommand](https://help.syncfusion.com/cr/cref_files/xamarin/Syncfusion.SfListView.XForms~Syncfusion.ListView.XForms.SfListView~HoldCommand.html) property with the item holding action from view model, where you can write navigation or any other action code in the execution. When defining the command, [ItemHoldingEventArgs](https://help.syncfusion.com/cr/cref_files/xamarin/Syncfusion.SfListView.XForms~Syncfusion.ListView.XForms.ItemHoldingEventArgs.html) will be passed as command parameter which has item information in execution.
+
+You can define the command parameter for the `HoldCommand` using [HoldCommandParameter](https://help.syncfusion.com/cr/cref_files/xamarin/Syncfusion.SfListView.XForms~Syncfusion.ListView.XForms.SfListView~HoldCommandParameter.html), where you can get the element reference passed in view model.
+
+```
+<syncfusion:SfListView x:Name="listView"                     
+                       HoldCommand="{Binding HoldCommand}"
+                       ItemsSource="{Binding BookInfoCollection}"/>
+```
+```
+//ViewModel.cs
+public class BookInfoRepository : INotifyPropertyChanged
+{
+    private Command holdCommand;
+
+    public Command HoldCommand
+    {
+        get { return holdCommand; }
+        protected set { holdCommand = value; }
+    }
+
+    public BookInfoRepository()
+    {
+        holdCommand = new Command(OnHold);
+    }
+
+    ///<summary>
+    /// Displays the item holding content
+    ///</summary>
+    public void OnHold(object obj)
+    {
+        var eventArgs = obj as Syncfusion.ListView.XForms.ItemHoldingEventArgs;           
+        Application.Current.MainPage.DisplayAlert("Type Of Item :" + eventArgs.ItemType, "Item Tapped Position : + " +eventArgs.Position , "Ok");
+    }
+}
+```
+To know more about MVVM in ListView, please refer our documentation [here](https://help.syncfusion.com/xamarin/sflistview/mvvm)
